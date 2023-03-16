71bd157c2a24646c8b1acd0b9cf7011467193e07
BunnyeNyash <otachibosibori.com>
2023-03-16 16:04:55 -0700

    c program that prints exactly xxxx followed by a new line, to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - c program that prints exactly xxxx followed by a new line, to the standard error
+ *
+ * Return: always 1
+ */
+
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
