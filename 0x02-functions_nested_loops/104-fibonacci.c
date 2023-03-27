bcb2b100910206102c27985afe250f90365d4c2f
BunnyeNyash <otachibosibori.com>
2023-03-27 03:36:56 -0700

    program that prints the first 98 Fibonacci numbers, starting with 1 and 2, separated by a comma followed by a space

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - Prints the first 98 Fibonacci numbers, starting with 1 and 2,
+ * separated by a comma followed by a space.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	unsigned long int a = 1, b = 2, c;
+	int i;
+
+	printf("%lu, %lu", a, b);
+	for (i = 3; i <= 98; i++)
+	{
+		c = a + b;
+		printf(", %lu", c);
+		a = b;
+		b = c;
+	}
+	printf("\n");
+
+	return (0);
+}
