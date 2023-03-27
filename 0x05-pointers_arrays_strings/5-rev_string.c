09fbb6408e96280c882b83b83125070bcea02f03
BunnyeNyash <otachibosibori.com>
2023-03-27 05:41:23 -0700

    function that reverses a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,17 +9,16 @@
 
 void rev_string(char *s)
 {
-	char temp = s[0];
+	char temp;
 	int i, len = 0;
 
 	while (s[len] != '\0')
 		len++;
 
-	for (i = 0; i < len; i++)
+	for (i = 0; i < len / 2; i++)
 	{
-		len--;
 		temp = s[i];
-		s[i] = s[len];
-		s[len] = temp;
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
 	}
 }
