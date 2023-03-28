74cb91de5e422672411af14e6d8a7bcce5544564
BunnyeNyash <otachibosibori.com>
2023-03-27 23:00:43 -0700

    print FizzBuzz for numbers multiples of both three and five

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
@@ -14,23 +15,17 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3) == 0 && (i % 5) == 0)
+		if (i % 15 == 0)
 			printf("FizzBuzz");
-
-		else if ((i % 3) == 0)
+		else if (i % 3 == 0)
 			printf("Fizz");
-
-		else if ((i % 5) == 0)
+		else if (i % 5 == 0)
 			printf("Buzz");
-
 		else
-			printf("%d ", i);
-
-		if (i == 100)
-			continue;
-		printf(" ");
+			printf("%i", i);
+		if (i < 100)
+			printf(" ");
 	}
 	printf("\n");
-
 	return (0);
 }
