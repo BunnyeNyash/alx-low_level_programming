c7cee098ab8cbca3c705250828cc5427cc2a16e7
BunnyeNyash <otachibosibori.com>
2023-03-30 14:35:26 -0700

    that the program prints a[2] = 98, followed by a new line

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -8,14 +8,17 @@
 
 int main(void)
 {
+	int n;
 	int a[5];
 	int *p;
 
-	a[2] = 9;
-	p = &a[2];
+	a[2] = 1024;
+	p = &n;
+
+	*(p + 5) = 98;
 
 	/* your code goes here */
-	printf("a[2] = %d\n", *(p));
+	printf("a[2] = %d\n", a[2]);
 
 	return (0);
 }
