7af8ac7cd8c2deb8a23f45b0d6273a320eb770b2
BunnyeNyash <otachibosibori.com>
2023-03-30 19:33:36 -0700

    that the program prints a[2] = 98, followed by a new line

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - prints the value of integer variables
+ * main - Entry point
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 
 int main(void)
@@ -23,11 +23,8 @@ int main(void)
 	 *   - only one statement
 	 *   - you are not allowed to code anything else than this line of code
 	 */
-
 	*(p + 5) = 98;
-
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
-
 	return (0);
 }
