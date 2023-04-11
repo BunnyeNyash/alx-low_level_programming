5ba995304f4d76a44fe03fe3317b167659633e73
BunnyeNyash <otachibosibori.com>
2023-04-10 19:45:21 -0700

    libmy.a file

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: string to print
+ *
+ * Return: Always 0 on success
+ */
+
+void _puts(char *s)
+{
+	while (*s != '\0')
+	{
+		_putchar(*s++);
+	}
+	_putchar('\n');
+}
